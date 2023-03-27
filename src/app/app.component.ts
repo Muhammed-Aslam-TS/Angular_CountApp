0e45208f3ecebc9c0e8472bbc2ba2b2ff51de787
muhammed aslam <taslam8797@gmail.com>
2023-03-27 10:38:18 +0530

    count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counting';
+  count = 0
+  number = ''
+  is_hidden = false
+
+
+
+  check(){
+    // this.count  >0 ? this.is_hidden =false : this.is_hidden =true
+    this.is_hidden = false
+    this.count  %2 ==0 ? this.number='EVEN' : this.number='ODD'
+    if (this.count == 0) {
+       this.number = 'empty'
+    }
+  }
+  reset(){
+    this.  is_hidden = true
+  }
 }
